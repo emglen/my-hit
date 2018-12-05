2f5d90463d113004601ccd2a1a189fa5b206a01e
Виталий <z3p4dces46ky>
2018-12-05 21:10:52 +0200

    add signinbutton locator

--- src/main/java/MainPage.java
+++ src/main/java/MainPage.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class MainPage {
@@ -5,4 +6,6 @@ public class MainPage {
     public MainPage(WebDriver driver){
         this.driver=driver;
     }
+    By signInButton= By.xpath("//div/ul/li/button");
+
 }
