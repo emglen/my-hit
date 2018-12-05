05e2a0bac33c0d9115d14d027faa13704a1a0f02
Виталий <z3p4dces46ky>
2018-12-05 21:20:11 +0200

    add searchbutton locator add 2 metods for buttons click

--- src/main/java/MainPage.java
+++ src/main/java/MainPage.java
@@ -6,6 +6,13 @@ public class MainPage {
     public MainPage(WebDriver driver){
         this.driver=driver;
     }
-    By signInButton= By.xpath("//div/ul/li/button");
+    private By signInButton= By.xpath("//div/ul/li/button");
+    private By searchButton=By.xpath("//div/a[@role='button']");
 
+    public void clickOnSignInButton(){
+        driver.findElement(signInButton).click();
+    }
+    public void clickOnSearchButton(){
+        driver.findElement(searchButton).click();
+    }
 }
