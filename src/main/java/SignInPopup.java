9a5e9d79461833772279e16dcfd0228da3149d83
Виталий <z3p4dces46ky>
2018-12-05 22:10:33 +0200

    first test

--- src/main/java/SignInPopup.java
+++ src/main/java/SignInPopup.java
@@ -1,8 +1,16 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class SignInPopup {
     private WebDriver driver;
     public SignInPopup(WebDriver driver){
         this.driver=driver;
+    }
+
+
+    public String getText(String text){
+        By loc=By.xpath(text);
+        String text1 =driver.findElement(loc).getText();
+        return text1;
     }
 }
--- src/test/java/MainPageTest.java
+++ src/test/java/MainPageTest.java
@@ -1,2 +1,33 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class MainPageTest {
+    WebDriver driver;
+    MainPage mainPageObj;
+    SignInPopup signInPopup;
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver","D:\\chromedriver.exe");
+        driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+        driver.get("https://my-hit.org");
+    }
+
+    @Test
+    public void loginPopupIsOpened(){
+        mainPageObj.clickOnSignInButton();
+        Assert.assertEquals(signInPopup.getText("//div/h3[@id='myModalLabel']"),"Вход или регистрация");
+    }
+
+    @After
+    public void endAllTest(){
+        driver.quit();
+    }
 }
