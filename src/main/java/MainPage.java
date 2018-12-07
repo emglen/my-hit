a874694dd14689be992ba1f617a1b221b937362a
emglen <emglen@ukr.net>
2018-12-07 13:19:23 +0200

    Update MainPage.java
    add search input locator
    add metod for a type text into search text box

--- src/main/java/MainPage.java
+++ src/main/java/MainPage.java
@@ -8,11 +8,15 @@ public class MainPage {
     }
     private By signInButton= By.xpath("//div/ul/li/button");
     private By searchButton=By.xpath("//div/a[@role='button']");
+    private By searchInput=By.xpath("//input[@id="search-navbar-q"]");
 
     public void clickOnSignInButton(){
         driver.findElement(signInButton).click();
     }
     public void clickOnSearchButton(){
         driver.findElement(searchButton).click();
+    }
+    public void typeTextIntoSearchTextbox(String text){
+        driver.findElement(searchInput).sendKeys(text);
     }
 }
