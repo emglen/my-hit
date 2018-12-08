92430395bb536fe967e2facac2315a8b1801c92c
Виталий <z3p4dces46ky>
2018-12-08 11:35:12 +0200

    add search result page add search result page test class

---
+++ src/main/java/SearchResultPage.java
@@ -0,0 +1,9 @@
+import org.openqa.selenium.WebDriver;
+
+public class SearchResultPage {
+    private WebDriver driver;
+    public SearchResultPage(WebDriver driver){
+        this.driver=driver;
+    }
+
+}
---
+++ src/test/java/SearchResultPageTest.java
@@ -0,0 +1,24 @@
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class SearchResultPageTest {
+    private WebDriver driver;
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver","D:\\chromedriver.exe");
+        driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+        driver.get("https://my-hit.org");
+    }
+
+
+
+    @After
+    public void endAllTest(){
+        driver.quit();
+    }
+}
